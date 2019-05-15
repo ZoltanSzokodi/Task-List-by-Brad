a3d5cec0a7d35bd88e900f265f677ebf1bccaf8f
Rupert <zoltan.szokodi@hotmail.com>
2019-05-15 14:31:54 +0200

    app.js Delete & Filter Tasks

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ loadEventListeners();
 function loadEventListeners() {
     // Add Task event
     form.addEventListener('submit', addTask);
+    // remove Task event
+    taskList.addEventListener('click', removeTask);
+    // Clear Task evet
+    clearBtn.addEventListener('click', clearTasks);
+    // Filter Tasks event
+    filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task 
@@ -40,7 +46,35 @@ function addTask(e) {
     // Clear input
     taskInput.value = '';
 
-
-
     e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains('delete-item')) {
+        if(confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+// Clear Tasks
+function clearTasks() {
+    while(taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+// Filter Tasks
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(task => {
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
 }
